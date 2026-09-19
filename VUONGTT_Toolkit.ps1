1f18d78e3c0afa1aa5eb6da0851389aa0ffb3c8f
truongthanhvuong <vuongtt@users.noreply.github.com>
2026-09-19 11:01:06 +0700

    release: v20.5.908.47 - fix 100% Vietnamese fonts, trigger update popup on v20.5.908.46 clients, push new binary to GitHub

--- VUONGTT_Toolkit.ps1
+++ VUONGTT_Toolkit.ps1
@@ -1,6 +1,6 @@
-﻿<#
+<#
 ========================================================================================
-   VUONGTT SOFTWARE - TOOLKIT 2026 VER 20.5.908.46
+   VUONGTT SOFTWARE - TOOLKIT 2026 VER 20.5.908.47
    VUONGTT Tool Pro 2026 - Professional
    Chuyên nghiệp - Tối ưu hóa - Cài đặt tự động - Sửa lỗi toàn diện Windows, Office & Phần cứng
 ========================================================================================
